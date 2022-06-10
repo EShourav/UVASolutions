73456eb059f2a118fda1f86fcbba2a2c85470221
Muntasher Morshed <ijeshourav1409@gmail.com>
2022-06-10 22:05:17 +0600

    C++, Adhoc

---
+++ 10071 - Back to High School Physics.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+#define sf scanf
+#define pf printf
+using namespace std;
+int main()
+{
+    int a,b;
+    while(sf("%d %d",&a, &b)!=EOF)
+    {
+        cout<<2*a*b<<endl;
+    }
+    return 0;
+}
